bebacc4727132bd24fa7b431bb360d72233bd451
Apple PC <applepc@ApplePCs-MacBook-Pro.local>
2018-10-08 10:45:35 +0500

    intial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getcurrentYear', () => {
+	return new Date().getFullYear()
+})
+hbs.registerHelper('screamIt' , (text) => {
+	return text.toUpperCase();
+})
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.originalUrl}`;
+	fs.appendFile('server.log', log + '\n', (err) =>{
+		if (err) {
+			console.log('Unable to append server log');
+		}
+	})
+	//console.log();
+	next();
+})
+
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to Home Page'
+	})
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Us Page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Unable to handle request'
+	})
+});
+
+app.listen(port, () => {
+	console.log(`server is up on ${port}`);
+});
